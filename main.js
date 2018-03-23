086ff006cd5f889079e3d0ed8d8feab08cda49c6
Soraya <Soraya@MacBook-Pro-van-Soraya-2.local>
2018-03-23 23:20:32 +0100

    IxD Challenge 2 - Soraya Santaris

--- main.js
+++ main.js
@@ -47,3 +47,20 @@ var mainHeading = document.getElementById('time');
 
 var tl1 = new TimelineMax();
 tl1.to(mainHeading, 1, {fontSize: 75});
+
+var blinkText = document.getElementById("messageText");
+
+function blink() {
+	TweenLite.to(blinkText, 0.3, {
+		autoAlpha: 0,
+		delay: 0.3,
+		onComplete: function() {
+			TweenLite.to(blinkText, 0.3, {
+				autoAlpha: 1,
+				delay: 0.3,
+				onComplete: blink
+			});
+		}
+	});
+}
+blink();
